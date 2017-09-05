41e65fb20bf6b96f949b9888a270193a1fb684c9
CF314 <CF314@users.noreply.github.com>
2017-09-05 19:52:17 +0200

    Add files via upload

---
+++ May Brainfuck/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace CF314.MayBrainfuck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Interpreter.RunFile(args[0]);
+            Environment.Exit(0);
+        }
+    }
+}
